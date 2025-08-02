890eb31fb995f5dc586a58666ce4532c787ca8c0
Aaron Brady <abrady@meta.com>
2025-08-02 10:35:10 -0700

    Add the bar examine data and dialogue

---
+++ src/examine/thebar.ts
@@ -0,0 +1,40 @@
+import { ExamineRect } from '../examine';
+
+export const thebarExamineRects: ExamineRect[] = [
+  {
+    x: 200,
+    y: 650,
+    width: 1100,
+    height: 250,
+    type: 'dialogue',
+    level: 'thebar',
+    dialogueNode: 'TheBar_Counter',
+  },
+  {
+    x: 50,
+    y: 300,
+    width: 200,
+    height: 300,
+    type: 'dialogue',
+    level: 'thebar',
+    dialogueNode: 'TheBar_Jukebox',
+  },
+  {
+    x: 600,
+    y: 350,
+    width: 400,
+    height: 200,
+    type: 'dialogue',
+    level: 'thebar',
+    dialogueNode: 'TheBar_Patrons',
+  },
+  {
+    x: 1300,
+    y: 250,
+    width: 200,
+    height: 400,
+    type: 'dialogue',
+    level: 'thebar',
+    dialogueNode: 'TheBar_Door',
+  },
+];
